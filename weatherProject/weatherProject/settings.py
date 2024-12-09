6e919aa23aff6ce5beefc4a24e11a1a8e7d22b09
amiiitabh <149476166+amiiitabh@users.noreply.github.com>
2024-12-09 06:05:11 +0530

    2nd commit

--- weatherProject/weatherProject/settings.py
+++ weatherProject/weatherProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&p!$lq228izo1af#pu1$*)4wo)_s-cqi79eddrou&*shbyja6!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*','.vercel.app   ']
 
 
 # Application definition
